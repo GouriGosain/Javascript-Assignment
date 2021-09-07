0e0eb34c2fcdbb4bdb5c8d64c53d84dc1fb5f7da
GouriGosain <52924864+GouriGosain@users.noreply.github.com>
2021-09-07 17:55:08 +0530

    JavaScript day1 assignment submitted

---
+++ jsday1/script.js
@@ -0,0 +1,30 @@
+// Binary operation
+var num1=70;
+var num2=50;
+var num3=60;
+// Arthmetic operators 
+// console.log(`sum of ${num1} and ${num2} is ${num1+num2}`);
+// console.log(`subtraction of ${num1} and ${num2} is ${num1-num2}`);
+// console.log(`product of ${num1} and ${num2} is ${num1*num2}`);
+// console.log(`quotient  of ${num1} and ${num2} is ${num1/num2}`);
+// console.log(`remainder of ${num1} and ${num2} is ${num1%num2}`);
+
+
+//Assignment operators
+// console.log(` ${num1} and ${num2} is num1+=num2  ${num1+=num2}`);
+// console.log(`  ${num1} and ${num2} is  num1-=num2  ${num1-=num2}`);
+// console.log(` ${num1} and ${num2} is num1*=num2 ${num1*=num2}`);
+// console.log(` ${num1} and ${num2} is num1/=num2 ${num1/=num2}`);
+// console.log(` ${num1} and ${num2} is num1%=num2 ${num1%=num2}`);
+
+//relational or comparision operators
+
+// console.log(` num1 = ${num1} is greator ${num1>=num2}`);
+// console.log(` num2 = ${num2} is greator ${num1<=num2}`);
+// console.log(` num1 = ${num1}  and num2 =${num2} is identical to each other  ${num1===num2}`);
+// console.log(` num1 = ${num1}  and num2 =${num2} is not  identical to each other  ${num1!==num2}`);
+
+//logical operators
+// console.log(num1<num2 || num2>num3);
+// console.log(num1<num2 && num2>num3);
+
